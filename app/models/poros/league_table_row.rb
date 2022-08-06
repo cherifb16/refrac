0dc0d0b6f3a8088293d2a24a892cf69e33c165ed
cherifb16 <cherifbobbyray@gmail.com>
2022-08-06 14:27:54 +0200

    fourth

--- app/models/poros/league_table_row.rb
+++ app/models/poros/league_table_row.rb
@@ -4,33 +4,35 @@ module Poros
     attr_accessor :club, :rank, :year, :matches
     def initialize(club, year)
       @club = club
-      
+      @year = year
+      @rank = nil
       @matches = club.matches_on(year)
     end
-      def digested_games_count
-        matches.where(kicked_off_at: Date.new(year, 1, 1)...Time.current).count
-      end
-      def win
-        club.win_on(year)
-      end
-      def lost
-        club.lost_on(year)
-      end
-      def draw
-        club.draw_on(year)
-      end
-      def goals
-        matches.sum (|match| match.goal_by(club) }
-      end
 
-      def goals_conceded
-        matches.sum (|match| match.goal_conceded_by(club) }
-      end
-      def goal difference
-        goals - goals_conceded
-      end
-      def points
-        win * League::WIN_POINTS + draw * League::DRAW_POINTS
-      end
+    def digested_games_count
+      matches.where(kicked_off_at: Date.new(year, 1, 1)...Time.current).count
+    end
+    def win
+      club.win_on(year)
+    end
+    def lost
+      club.lost_on(year)
+    end
+    def draw
+      club.draw_on(year)
+    end
+    def goals
+      matches.sum (|match| match.goal_by(club) }
+    end
+      
+    def goals_conceded
+      matches.sum (|match| match.goal_conceded_by(club) }
+    end
+    def goal difference
+      goals - goals_conceded
+    end
+    def points
+      win * League::WIN_POINTS + draw * League::DRAW_POINTS
+    end
   end
 end
